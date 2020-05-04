1764ff0cf74010157b4a67d1f83a79b6094a6677
STEWMACD <stewart.d.macdonald@gmail>
2020-05-03 20:27:48 -0700

    commit on 'master'

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Stewart MacDonald`,
+    description: `A resume and portfolio for Stewart MacDonald`,
+    author: `Stewart MacDonald`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
