5eefc2d619e34cee7dc11fc01f9b5b8b066af8a4
STEWMACD <stewart.d.macdonald@gmail>
2020-05-04 08:21:33 -0700

    a

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,22 +13,18 @@ module.exports = {
         path: `${__dirname}/src/`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    'gatsby-transformer-json',
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        path: './src/contentData',
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: 'gatsby-transformer-json',
+      options: {
+        typeName: "Json"
+      }
+    }
   ],
 }
